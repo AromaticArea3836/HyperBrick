4280b77285881304a6f2688d0401cceb8a4ad07b
rickastleey <150521365+xnggypr@users.noreply.github.com>
2025-04-14 19:51:37 +0200

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -2,6 +2,8 @@
 ui_print ""
 ui_print "- HyperBrick: feel greatness of your animations on low-end device on HyperOS 2."
 ui_print ""
+. $MODPATH/utils.sh
+    restore_deviceLevelList
+ui_print ""
 ui_print "- Sorry see you go. Thanks for using HyperBreak module."
-rm -rf /data/resource-cache/* /data/system/package_cache/* /cache/* /data/dalvik-cache/*
 abort "- ✅ Done! Reboot your device to delete this module fully."
